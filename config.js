86bc9669a09a3e84c6a5c5e109e5cf47474c6dc2
amir26211 <84690164+amir26211@users.noreply.github.com>
2021-05-28 11:20:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/a9SHDpD", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
+  Token: process.env.Token || "ODI4OTIxNDYzMzQ4MjY0OTgy.YGwnRQ.xGVjb6HnzzOATZm1iZ8skleBASk", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
@@ -30,3 +30,4 @@ module.exports = {
     ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
   },
 };
+ODI4OTIxNDYzMzQ4MjY0OTg
